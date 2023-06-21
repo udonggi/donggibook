d1ac3ac2d03d556e21fcfb99adf5ea1037d86937
udonggi <dongki1882@naver.com>
2023-06-21 11:47:07 +0900

    MariaDB -> MysqlDB 로 바꾸고 test에서 QueryDSL 사용해서 H2 쓰지 않기로

--- db/init.sql
+++ db/init.sql
@@ -1,3 +1,8 @@
+# 테스트 DB 생성
+DROP DATABASE IF EXISTS donggibook__test;
+CREATE DATABASE donggibook__test;
+USE donggibook__test;
+
 # 개발 DB 생성
 DROP DATABASE IF EXISTS donggibook__dev;
 CREATE DATABASE donggibook__dev;
